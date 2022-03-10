dac814296f03d493969459b394743cb1d642f36f
katman-Generation <tkatema5@gmail.com>
2022-03-10 13:44:01 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints Use the function printf
+ * Your program should return 0
+ * Return: 0 if exited propery , non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
