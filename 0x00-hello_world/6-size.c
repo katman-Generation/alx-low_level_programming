509ed2298db770c85f5452280f98921dd1bffacf
katman-Generation <tkatema5@gmail.com>
2022-03-10 15:16:01 -0800

    my my my my

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the zise of various types
  * Description: Warnings are allowed
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
