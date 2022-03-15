009f1000cd45c30a6f3e0374112d277e9dcd323b
katman-Generation <tkatema5@gmail.com>
2022-03-15 12:46:36 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(word[i]);
+		i++;
+	
+	}
+	_putchar('\n');
+	return (0);
+}
