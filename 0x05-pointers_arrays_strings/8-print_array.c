b795e3d12a2b747e61912fc841e57224d66f8315
katman-Generation <tkatema5@gmail.com>
2022-03-22 13:57:41 -0700

    submitting

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,7 +6,7 @@
  *
  * @a: array to be printed
  * @n: number of elements to print
- * 
+ *
  * Return: void
  */
 void print_array(int *a, int n)
@@ -14,7 +14,6 @@ void print_array(int *a, int n)
 	int i;
 
 	if (n > 0)
-	
 		printf("%d", a[0]);
 	for (i = 1; i < n; i++)
 	{
