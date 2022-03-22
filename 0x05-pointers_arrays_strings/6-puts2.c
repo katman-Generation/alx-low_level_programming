e7d32f7115dde3fe86f4e17b09b9fc82f9d3348e
katman-Generation <tkatema5@gmail.com>
2022-03-22 13:23:58 -0700

    submitting

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints str to standard output in reverse + new line
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int str_size;
+
+
+	str_size = strlen(str);
+
+	for (i = 0; i < str_size; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
