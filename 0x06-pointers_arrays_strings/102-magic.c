eda9bf7089b0627a1d9a52f230a975cf321de3f1
katman-Generation <tkatema5@gmail.com>
2022-03-25 07:11:43 -0700

    submitting

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,11 +5,11 @@ int main(void)
 	int n;
 	int a[5];
 	int *p;
-	
+
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here...
+	 * write your line of the code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
