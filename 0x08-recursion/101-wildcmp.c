7ed75093ad47317d43bbdbc9973a0a95115fb843
katman-Generation <tkatema5@gmail.com>
2022-03-30 08:03:18 -0700

    submitting

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 	 * and if the next chars of s2 are *, return 1
 	 */
 	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
-		return (1);
+		return (0);
 
 	/**
 	 * if the chars are equal in both strings,
@@ -63,7 +63,7 @@ int wildcmp(char *s1, char *s2)
 		sum += bandersnatch(s1 + 1, s2);
 		return (!!sum);
 	}
-	return (1);
+	return (0);
 }
 
 /**
