b3fd3169c71b0362f8196f37aa047fd6b09afee7
katman-Generation <tkatema5@gmail.com>
2022-04-05 06:54:18 -0700

    submitting

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,15 +13,16 @@ char *argstostr(int ac, char **av)
 	int i, j, k, len;
 	char *str;
 
-	if (ac == 0 || av == NULL)
+	if (ac <= 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			len++;
 		len++;
 	}
-	str = malloc(sizeof(char));
+	len++;
+	str = malloc(sizeof(char) * len);
 
 	if (str == NULL)
 		return (NULL);
@@ -30,7 +31,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
 			str[k] = av[i][j];
 			k++;
@@ -38,5 +39,6 @@ char *argstostr(int ac, char **av)
 		str[k] = '\n';
 		k++;
 	}
+	str[k] = '\0';
 	return (str);
 }
