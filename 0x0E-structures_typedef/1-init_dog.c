919a424d783e3a9ad560d7eeab67c7925da26b43
katman-Generation <tkatema5@gmail.com>
2022-04-11 23:35:04 -0700

    submitting

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * init_dog - initializes struct dog
+ * @d: adress of struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+	{
+		return;
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner
+}
