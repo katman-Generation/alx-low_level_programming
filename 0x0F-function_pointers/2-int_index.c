d8a4848ca1fc94f81a90be8a892062f47497d8eb
katman-Generation <tkatema5@gmail.com>
2022-04-13 13:38:41 -0700

    submitting

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (array && cmp)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
-	}
+	if (cmp == NULL || array == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	if (i == size)
+		return (-1);
+
 	return (-1);
 }
