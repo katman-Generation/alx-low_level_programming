d0550741677b94948a2bfe4fc7d806ccec35228c
katman-Generation <tkatema5@gmail.com>
2022-04-14 04:42:42 -0700

    submitting

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,6 @@ int sum_them_all(const unsigned int n, ...)
 	va_list num;
 	int sum;
 
-	if (n == 0)
-		return (0);
-
 	va_start(list, n);
 	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(list, int);
