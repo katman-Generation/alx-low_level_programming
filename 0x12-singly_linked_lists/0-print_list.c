0a30cadb94db4b0831f40db0e47e5b3a15d2c331
katman-Generation <tkatema5@gmail.com>
2022-04-21 23:48:04 -0700

    submitting

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdio.hi>
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -18,9 +18,9 @@ size_t print_list(const list_t *h)
 		else
 		{
 			printf(" [%u] $s \n", h->len, h->str);
-			h = h->next;
-			p++;
 		}
+		h = h->next;
+		p++;
 	}
 	return (p);
 }
