11a94dcd471e1d1e7a8153d9ed4d16f66c5826ce
katman-Generation <tkatema5@gmail.com>
2022-04-26 14:22:47 -0700

    submitting

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	NewNode = malloc(sizeof(listint_t));
 
-	if (NewNode == NULL)
+	if (!NewNode || !head)
 		return (NULL);
 
 	NewNode->n = n;
