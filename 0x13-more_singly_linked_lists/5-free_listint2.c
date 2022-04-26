6d6cb78bbfe00abd2d1b7928eebb880b09e9736c
katman-Generation <tkatema5@gmail.com>
2022-04-26 00:01:58 -0700

    submitting

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 	if (head == Null)
 		return;
 
+	temp = *head
+
 	while (*head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
 	}
 	*head = NULL;
 }
