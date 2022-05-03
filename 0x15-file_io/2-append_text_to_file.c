3a504908327ed6ee5ad64af24461b2d5b2daf4f2
katman-Generation <tkatema5@gmail.com>
2022-05-03 12:00:50 -0700

    submitting

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while ( text_content[n])
+		while (text_content[n])
 			n++;
 		t = write(fd, text_content, n);
 		if (t != n)
