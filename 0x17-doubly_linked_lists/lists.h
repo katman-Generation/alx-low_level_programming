c4b23bb39ee89768cbaab4c04098eb29b1db55fe
katman-Generation <tkatema5@gmail.com>
2022-06-09 13:09:27 -0700

    submitting

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Holberton project
+ * 
  */
 typedef struct dlistint_s
 {
@@ -19,5 +19,5 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-size_t print_dlistint(const dlistint_t *h);
 
+size_t print_dlistint(const dlistint_t *h);
