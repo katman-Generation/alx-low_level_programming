ad6690154a3d4c3fb995d360ded3fb9635e3656f
katman-Generation <tkatema5@gmail.com>
2022-06-09 23:54:42 -0700

    submmitting

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/*
+ * print_dlistint - prints a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}	
