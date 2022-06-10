d200f0c560b0109e9a8eb876a4a52b33f9b28136
katman-Generation <tkatema5@gmail.com>
2022-06-10 00:36:04 -0700

    submitting

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "list.h"
+
+/**
+ * add_dnodeint - function
+ * @head: double pointer to first node
+ * @n: new node data to add
+ *
+ * Description: function to add new node to beginning of dbl lnkd list
+ * Return: Address of new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *NewNode;
+	int x = n;
+
+	NewNode = malloc(sizeof(dlistint_t));
+
+	if (NeweNode == NULL)
+		return (NULL);
+
+	NewNode->n = x;
+	NewNode->next = *head;
+	NewNode->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = NewNode;
+	*head = NewNode;
+	return (NewNode);
+}
